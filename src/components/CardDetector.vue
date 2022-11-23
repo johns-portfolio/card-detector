f421775ffcd63d566841601fd7aa945bbbf8d3ad
Apisit Sianglert <apisit.sianglert@lotuss.com>
2022-11-23 15:16:37 +0700

    Book detection

--- src/components/CardDetector.vue
+++ src/components/CardDetector.vue
@@ -1,57 +1,90 @@
 <script setup lang="ts">
-import { Ref, ref, watch } from 'vue'
+import { onMounted, Ref, ref, watch } from 'vue'
+
+import '@tensorflow/tfjs-backend-cpu'
+import '@tensorflow/tfjs-backend-webgl'
+import * as cocoSsd from '@tensorflow-models/coco-ssd'
 
 const cameraRef: Ref<HTMLVideoElement> = ref<HTMLVideoElement>() as any
 const canvasRef: Ref<HTMLCanvasElement> = ref<HTMLCanvasElement>() as any
+const canvasRef2: Ref<HTMLCanvasElement> = ref<HTMLCanvasElement>() as any
 
-function getImageFromCamera() {
-  const canvas = canvasRef.value
-  const context = canvas.getContext('2d')!
+onMounted(async () => {
+  const model = await cocoSsd.load()
   const video = cameraRef.value
+  const canvas = canvasRef.value
+  const ctx = canvas.getContext('2d')!
+  const ctx2 = canvasRef2.value.getContext('2d')!
 
-  context.drawImage(video, 0, 0, 100, 100)
-  return canvas.toDataURL('image/png')
-}
+  const renderPrediction = async () => {
+    const predictions = await model.detect(video)
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.drawImage(video, 0, 0, ctx.canvas.width, ctx.canvas.height)
 
-function loadImageToCanvas() {
-  getImageFromCamera()
+    // Draw the bounding box.
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0]
+      const y = prediction.bbox[1]
+      const width = prediction.bbox[2]
+      const height = prediction.bbox[3]
 
-  requestAnimationFrame(loadImageToCanvas)
-}
+      // Draw the bounding box.
+      ctx.strokeStyle = '#00FFFF'
+      ctx.lineWidth = 4
+      ctx.strokeRect(x, y, width, height)
 
-function openCamera() {
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then((stream) => {
-      if (cameraRef) {
-        cameraRef.value.srcObject = stream
-        cameraRef.value.onloadedmetadata = () => {
-          cameraRef.value.width = cameraRef.value.videoWidth
-          cameraRef.value.height = cameraRef.value.videoHeight
+      // Draw the label 
+      ctx.font = '18px Arial'
+      ctx.fillStyle = '#00FFFF'
+      ctx.fillText(prediction.class, x, y)
 
-          requestAnimationFrame(loadImageToCanvas)
-        }
+    })
+
+    const bookPrediction = predictions.filter((c) => c.class === 'book')[0]
+    if (bookPrediction) {
+      const [x, y, width, height] = bookPrediction.bbox
+
+      const croppedImage = ctx.getImageData(x, y, width, height)
+      ctx2.putImageData(croppedImage, 0, 0)
+    }
+    requestAnimationFrame(renderPrediction)
+  }
+
+  const startVideo = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: 'environment'
       }
     })
-    .catch((err) => {
-      console.log(
-        "May the browser didn't support or there is some errors.",
-        err
-      )
-    })
-}
+    video.srcObject = stream
+    video.onloadedmetadata = async () => {
+      cameraRef.value.width = cameraRef.value.videoWidth
+      cameraRef.value.height = cameraRef.value.videoHeight
+      canvasRef.value.width = cameraRef.value.videoWidth
+      canvasRef.value.height = cameraRef.value.videoHeight
+      video.play()
+      await renderPrediction()
+    }
+  }
+
+  await startVideo()
+})
 </script>
 
 <template>
-  <div class="h-screen p-10">
+  <div class="h-screen p-10 grid grid-cols-2">
     <div class="bg-purple-400">
-      <video ref="cameraRef" :width="450" :height="337.5" autoplay></video>
+      <video ref="cameraRef" autoplay></video>
     </div>
     <div class="bg-pink-400">
       <canvas ref="canvasRef"></canvas>
     </div>
+    <div class="bg-pink-400">
+      <canvas ref="canvasRef2"></canvas>
+    </div>
     <div>
-      <button @click="openCamera">Open Camera</button>
+      <!-- <button @click="openCamera">Open Camera</button> -->
     </div>
   </div>
 </template>
